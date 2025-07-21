d9e7ccdc656ed4bda04bc4db056c4747b23d4974
gowthamramesh-dev <goethamramesh199@gmail.com>
2025-07-21 12:08:08 +0530

    some ui changes

--- utils/sendWhatsAppTemplate.js
+++ utils/sendWhatsAppTemplate.js
@@ -9,9 +9,9 @@ const client = require("twilio")(accountSid, authToken);
 client.messages
   .create({
     from: "whatsapp:+14155238886",
-    contentSid: "HXb5b62575e6e4ff6129ad7c8efe1f983e",
+    contentSid: "",
     contentVariables: '{"1":"12/1","2":"3pm"}',
-    to: "whatsapp:+919384565239",
+    to: "whatsapp:+91**********",
   })
   .then((message) => console.log(message.sid))
   .done();
